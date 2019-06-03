deb0544959c7f38871727528aaabfe7835b863dc
Doug Arcuri <xsurrealx@hotmail.com>
2019-06-03 19:54:23 -0400

    Add authors view and controller

---
+++ ef-core-web-demo/Controllers/AuthorController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreWebDemo.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using (var context = new EFCoreWebDemoContext())
+            {
+                var model = await context.Authors.AsNoTracking().ToListAsync();
+                return View(model);
+            }
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FirstName, LastName")] Author author)
+        {
+            using (var context = new EFCoreWebDemoContext())
+            {
+                context.Add(author);
+                await context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
