56f2d2a67a2670f5dfd875a89cf95937bea24d8a
Doug Arcuri <xsurrealx@hotmail.com>
2019-06-03 19:20:25 -0400

    Update models with StringLimit

--- ef-core-web-demo/Models/Book.cs
+++ ef-core-web-demo/Models/Book.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EFCoreWebDemo
 {
     public class Book
     {
         public int BookId { get; set; }
+        [StringLength(255)]
         public string Title { get; set; }
         public int AuthorId { get; set; }
         public Author Author { get; set; }
